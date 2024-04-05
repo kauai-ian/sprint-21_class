c8b6dab561b1cd21f5fe9765770965067095be76
Andrew Locke <andrew.locke@spiral.us>
2024-04-04 19:47:51 -1000

    Adds message routes and implements

--- message-app/server/app.js
+++ message-app/server/app.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
-const createError = require('http-errors');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config();
+const createError = require("http-errors");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // Connect to db
 mongoose
@@ -17,35 +17,35 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
-
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
 // ROUTES
-const userRoutes = require('./routes/user.routes');
+const userRoutes = require("./routes/user.routes");
+const messageRoutes = require("./routes/message.routes");
 
-app.use('/users', userRoutes);
-
+app.use("/users", userRoutes);
+app.use("/messages", messageRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ message-app/server/routes/message.routes.js
@@ -0,0 +1,10 @@
+const messages = require("../controllers/messages.controller.js");
+const router = require("express").Router();
+
+router.post("/", messages.createMessage);
+router.get("/", messages.listMessages);
+router.get("/:_id", messages.getMessage);
+router.put("/:_id", messages.likeMessage);
+router.delete("/:_id", messages.deleteMessage);
+
+module.exports = router;
