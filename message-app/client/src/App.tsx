77cad57cabc93c5b726dd6650850ba198f0b35f4
Andrew Locke <andrew.locke@spiral.us>
2024-04-04 20:29:06 -1000

    Set top level loading

--- message-app/client/src/App.tsx
+++ message-app/client/src/App.tsx
@@ -5,24 +5,42 @@ import Home from "./pages/Home";
 import Feed from "./pages/Feed";
 import Callback from "./pages/Callback";
 import AuthRoute from "./components/AuthRoute";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Center, Container, Spinner } from "@chakra-ui/react";
 
 function App() {
+  const { isLoading } = useAuth0();
+
   return (
     <Layout>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/:sub" element={
-          <AuthRoute>
-            <Profile />
-          </AuthRoute>
-        } />
-        <Route path="/callback" element={
-          <AuthRoute>
-            <Callback />
-          </AuthRoute>
-        } />
-        <Route path="/feed" element={<Feed />} />
-      </Routes>
+      {isLoading ? (
+        <Container h="100%">
+          <Center mt="100px">
+            <Spinner />
+          </Center>
+        </Container>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/profile/:sub"
+            element={
+              <AuthRoute>
+                <Profile />
+              </AuthRoute>
+            }
+          />
+          <Route
+            path="/callback"
+            element={
+              <AuthRoute>
+                <Callback />
+              </AuthRoute>
+            }
+          />
+          <Route path="/feed" element={<Feed />} />
+        </Routes>
+      )}
     </Layout>
   );
 }
